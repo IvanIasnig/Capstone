164426e9e3177793ce0762a30a7743d7835f2bfa
ivan1998iasnig@gmail.com <ivan1998iasnig@gmail.com>
2023-09-06 14:14:13 +0200

    controller

---
+++ server/ivaniasnig/src/main/java/fitnessproject/ivaniasnig/user/UserController.java
@@ -0,0 +1,51 @@
+package fitnessproject.ivaniasnig.user;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/user")
+@RestController
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	//GET
+	@GetMapping
+	public Page <User> getUtenti(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size, @RequestParam(defaultValue = "id") String sortBy) {
+		return userService.find(page, size, sortBy);
+	}
+	
+	
+	// GET (by Id)
+	@GetMapping("/{id}")
+	public User findByUsername(@PathVariable UUID id) { 
+		return userService.findById(id);
+	}
+	
+	
+	// PUT 
+	@PutMapping("/{id}")
+	public User findAndUpdate(@PathVariable UUID id, @RequestBody User body) {
+		return userService.findByIdAndUpdate(id,body);
+	}
+	
+	// DELETE
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void findAndDelete(@PathVariable UUID id) {
+		userService.findByIdAndDelete(id);
+	}
+}
--- server/ivaniasnig/src/main/java/fitnessproject/ivaniasnig/user/UserService.java
+++ server/ivaniasnig/src/main/java/fitnessproject/ivaniasnig/user/UserService.java
@@ -1,7 +1,16 @@
 package fitnessproject.ivaniasnig.user;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Pageable;
+
+import exceptions.BadRequestException;
+import exceptions.NotFoundException;
 
 @Repository
 public class UserService {
@@ -9,7 +18,45 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepo;
 	
-//	public User save(User user) {
-//		if(user.getPassword().length() < 6)
-//	}
+	public User save(User user) {
+		if(user.getPassword().length() < 6) {
+			throw new BadRequestException("The password must contain at least 6 characters");
+		}
+		userRepo.findByEmail(user.getMail()).ifPresent(usr -> {
+			throw new BadRequestException("email already in use");
+		});
+		
+		return userRepo.save(user);
+	}
+	
+	public Page<User> find(int page, int size, String sort){
+		Pageable pageable=PageRequest.of(page, size, Sort.by(sort));
+		return userRepo.findAll(pageable);
+	}
+	
+	public User findById(UUID id) throws NotFoundException {
+		return userRepo.findById(id).orElseThrow(() -> new NotFoundException(id));
+	}
+	
+	public User findByIdAndUpdate(UUID id, User user) throws NotFoundException {
+		User found = this.findById(id);
+		found.setSurname(user.getSurname());
+		found.setName(user.getName());
+		found.setAge(user.getAge());
+		found.setSex(user.getSex());
+		found.setUsername(user.getMail());
+		found.setMail(user.getMail());
+		
+		return userRepo.save(found);
+	}
+	
+	public void findByIdAndDelete(UUID id) throws NotFoundException {
+		User found = this.findById(id);
+		userRepo.delete(found);
+	}
+	
+	public User findByEmail(String email) {
+		return userRepo.findByEmail(email).orElseThrow(() -> new NotFoundException("User not found"));
+	}
+	
 }
