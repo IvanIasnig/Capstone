3a3204dbe1c97f3b3be77c2da8c3da7962f91882
ivan1998iasnig@gmail.com <ivan1998iasnig@gmail.com>
2023-09-09 02:52:57 +0200

    edamam api tested

--- client/src/screen/UserProfile.js
+++ client/src/screen/UserProfile.js
@@ -1,9 +1,14 @@
 import { useAuth } from "../provider/AuthProvider";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function UserProfile() {
   const { name } = useAuth();
+  const [apiData, setApiData] = useState(null);
+  const [error, setError] = useState(null);
+  const [foodQuery, setFoodQuery] = useState(""); // <-- Added state for food input
 
-  const storedRof = JSON.parse(localStorage.getItem("restOfData"));
+  const storedRof = JSON.parse(localStorage.getItem("restOfData")) || {};
 
   function Kcal(data) {
     let bmr = 0;
@@ -24,10 +29,49 @@ function UserProfile() {
     return res.toString();
   }
 
+  const KEY = "d802a615ff134b103d4b0a29cdf27e04";
+  const ID = "43f50be8";
+
+  const BASE_URL = `https://api.edamam.com/api/nutrition-data?app_id=${ID}&app_key=${KEY}&nutrition-type=cooking&ingr=${foodQuery}`;
+
+  async function fetchNutritionInfo() {
+    try {
+      const response = await axios.get(BASE_URL);
+      console.log(response.data);
+      setApiData(response.data);
+    } catch (error) {
+      console.error("Errore durante la chiamata API:", error);
+      setError(error);
+    }
+  }
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    fetchNutritionInfo();
+  };
+
   return (
     <div>
       <h1>Benvenuto, {name}!</h1>
       <div>le tue kcal giornaliere sono: {Kcal(storedRof)}</div>
+      {/* Input field for food query */}
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          placeholder="Enter food item..."
+          value={foodQuery}
+          onChange={(e) => setFoodQuery(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      {error && <p>Si è verificato un errore: {error.message}</p>}
+      {/* Assuming apiData has a property named `name` */}
+      {apiData && (
+        <div>
+          <h2>{apiData.name}</h2>
+          <p>Calories: {apiData.calories}</p>
+        </div>
+      )}
     </div>
   );
 }
