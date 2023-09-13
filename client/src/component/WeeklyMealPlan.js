04f4384d9d0506a324be74e7cd685b89f9aea114
ivan1998iasnig@gmail.com <ivan1998iasnig@gmail.com>
2023-09-14 01:44:53 +0200

    form changed

--- client/src/component/WeeklyMealPlan.js
+++ client/src/component/WeeklyMealPlan.js
@@ -4,6 +4,57 @@ import jwtDecode from "jwt-decode";
 
 const WeeklyMealPlan = () => {
   const [response, setResponse] = useState(null);
+  const [dayDiets, setDayDiets] = useState({
+    Monday: {
+      breakfast: { food: "", grams: 0 },
+      morningSnack: { food: "", grams: 0 },
+      lunch: { food: "", grams: 0 },
+      afternoonSnack: { food: "", grams: 0 },
+      dinner: { food: "", grams: 0 },
+    },
+    Tuesday: {
+      breakfast: { food: "", grams: 0 },
+      morningSnack: { food: "", grams: 0 },
+      lunch: { food: "", grams: 0 },
+      afternoonSnack: { food: "", grams: 0 },
+      dinner: { food: "", grams: 0 },
+    },
+    Wednesday: {
+      breakfast: { food: "", grams: 0 },
+      morningSnack: { food: "", grams: 0 },
+      lunch: { food: "", grams: 0 },
+      afternoonSnack: { food: "", grams: 0 },
+      dinner: { food: "", grams: 0 },
+    },
+    Thursday: {
+      breakfast: { food: "", grams: 0 },
+      morningSnack: { food: "", grams: 0 },
+      lunch: { food: "", grams: 0 },
+      afternoonSnack: { food: "", grams: 0 },
+      dinner: { food: "", grams: 0 },
+    },
+    Friday: {
+      breakfast: { food: "", grams: 0 },
+      morningSnack: { food: "", grams: 0 },
+      lunch: { food: "", grams: 0 },
+      afternoonSnack: { food: "", grams: 0 },
+      dinner: { food: "", grams: 0 },
+    },
+    Saturday: {
+      breakfast: { food: "", grams: 0 },
+      morningSnack: { food: "", grams: 0 },
+      lunch: { food: "", grams: 0 },
+      afternoonSnack: { food: "", grams: 0 },
+      dinner: { food: "", grams: 0 },
+    },
+    Sunday: {
+      breakfast: { food: "", grams: 0 },
+      morningSnack: { food: "", grams: 0 },
+      lunch: { food: "", grams: 0 },
+      afternoonSnack: { food: "", grams: 0 },
+      dinner: { food: "", grams: 0 },
+    },
+  });
 
   // Recupera il token dal localStorage
   const authToken = localStorage.getItem("authToken");
@@ -14,13 +65,18 @@ const WeeklyMealPlan = () => {
 
     try {
       const decoded = jwtDecode(token);
-      console.log(decoded);
       return decoded.sub;
     } catch (err) {
       console.error("Errore durante la decodifica del token", err);
       return null;
     }
   }
+
+  const handleInputChange = (day, meal, type, value) => {
+    const updatedDayDiets = { ...dayDiets };
+    updatedDayDiets[day][meal][type] = value;
+    setDayDiets(updatedDayDiets);
+  };
 
   const postData = async () => {
     const userId = getUserIdFromToken();
@@ -30,171 +86,7 @@ const WeeklyMealPlan = () => {
     }
     const url = `http://localhost:3001/user/diet/registerDiet?userId=${userId}`;
 
-    const data = {
-      dayDiets: [
-        {
-          dayName: "Monday",
-          breakfast: {
-            food: "Pane e marmellata",
-            grams: 300,
-          },
-          morningSnack: {
-            food: "Mela",
-            grams: 100,
-          },
-          lunch: {
-            food: "Spaghetti al pomodoro",
-            grams: 500,
-          },
-          afternoonSnack: {
-            food: "Yogurt",
-            grams: 150,
-          },
-          dinner: {
-            food: "Insalata mista",
-            grams: 250,
-          },
-        },
-        {
-          dayName: "Tuesday",
-          breakfast: {
-            food: "Pane e marmellata",
-            grams: 300,
-          },
-          morningSnack: {
-            food: "Mela",
-            grams: 100,
-          },
-          lunch: {
-            food: "Spaghetti al pomodoro",
-            grams: 500,
-          },
-          afternoonSnack: {
-            food: "Yogurt",
-            grams: 150,
-          },
-          dinner: {
-            food: "Insalata mista",
-            grams: 250,
-          },
-        },
-        {
-          dayName: "Wednesday",
-          breakfast: {
-            food: "Pane e marmellata",
-            grams: 300,
-          },
-          morningSnack: {
-            food: "Mela",
-            grams: 100,
-          },
-          lunch: {
-            food: "Spaghetti al pomodoro",
-            grams: 500,
-          },
-          afternoonSnack: {
-            food: "Yogurt",
-            grams: 150,
-          },
-          dinner: {
-            food: "Insalata mista",
-            grams: 250,
-          },
-        },
-        {
-          dayName: "Thursday",
-          breakfast: {
-            food: "Pane e marmellata",
-            grams: 300,
-          },
-          morningSnack: {
-            food: "Mela",
-            grams: 100,
-          },
-          lunch: {
-            food: "Spaghetti al pomodoro",
-            grams: 500,
-          },
-          afternoonSnack: {
-            food: "Yogurt",
-            grams: 150,
-          },
-          dinner: {
-            food: "Insalata mista",
-            grams: 250,
-          },
-        },
-        {
-          dayName: "Friday",
-          breakfast: {
-            food: "Pane e marmellata",
-            grams: 300,
-          },
-          morningSnack: {
-            food: "Mela",
-            grams: 100,
-          },
-          lunch: {
-            food: "Spaghetti al pomodoro",
-            grams: 500,
-          },
-          afternoonSnack: {
-            food: "Yogurt",
-            grams: 150,
-          },
-          dinner: {
-            food: "Insalata mista",
-            grams: 250,
-          },
-        },
-        {
-          dayName: "Saturday",
-          breakfast: {
-            food: "Pane e marmellata",
-            grams: 300,
-          },
-          morningSnack: {
-            food: "Mela",
-            grams: 100,
-          },
-          lunch: {
-            food: "Spaghetti al pomodoro",
-            grams: 500,
-          },
-          afternoonSnack: {
-            food: "Yogurt",
-            grams: 150,
-          },
-          dinner: {
-            food: "Insalata mista",
-            grams: 250,
-          },
-        },
-        {
-          dayName: "Sunday",
-          breakfast: {
-            food: "Pane e marmellata",
-            grams: 300,
-          },
-          morningSnack: {
-            food: "Mela",
-            grams: 100,
-          },
-          lunch: {
-            food: "Spaghetti al pomodoro",
-            grams: 500,
-          },
-          afternoonSnack: {
-            food: "Yogurt",
-            grams: 150,
-          },
-          dinner: {
-            food: "Insalata mista",
-            grams: 250,
-          },
-        },
-      ],
-    };
+    const data = { dayDiets: Object.values(dayDiets) };
 
     try {
       const config = {
@@ -212,6 +104,32 @@ const WeeklyMealPlan = () => {
 
   return (
     <div>
+      {Object.entries(dayDiets).map(([day, meals]) => (
+        <div key={day}>
+          <h3>{day}</h3>
+          {Object.entries(meals).map(([meal, details]) => (
+            <div key={meal}>
+              <label>{meal}</label>
+              <input
+                type="text"
+                placeholder="Cibo"
+                value={details.food}
+                onChange={(e) =>
+                  handleInputChange(day, meal, "food", e.target.value)
+                }
+              />
+              <input
+                type="number"
+                placeholder="Grammi"
+                value={details.grams}
+                onChange={(e) =>
+                  handleInputChange(day, meal, "grams", e.target.value)
+                }
+              />
+            </div>
+          ))}
+        </div>
+      ))}
       <button onClick={postData}>Invia Piano Alimentare</button>
       {response && <div>Risposta del server: {JSON.stringify(response)}</div>}
     </div>
